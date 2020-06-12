2b6067047a4b374983727e53267b8326eea59763
Oleksiy Dubovyk <dubovyk@gmail.com>
2020-06-12 13:29:46 +0300

    feat(1): investigate component code sharing between React and React Native

---
+++ packages/mobile/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+
+import {App} from 'common/src/App';
+
+AppRegistry.registerComponent(appName, () => App);
