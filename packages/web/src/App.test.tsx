24504140c6465159c033c77a1a3b24afa5ff7d6f
Oleksandr Biliaiev <Oleksandr_Biliaiev@epam.com>
2020-06-17 17:35:58 +0300

    feat(4): unit test coverage

--- packages/web/src/App.test.tsx
+++ packages/web/src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+it('Enzyme renders without crashing', () => {
+  shallow(<App />);
+});
+
