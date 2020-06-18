5c569f2ccea1761b6f25c719a3b4ffd77359fde8
Vladyslav Huz <wlad792@gmail.com>
2020-06-18 19:31:57 +0300

    [BE] feat(3): added GET /api/health-check endpoint

---
+++ packages/server/src/exceptions/health-check.exception.ts
@@ -0,0 +1,7 @@
+import HttpException from './http.exception';
+
+export default class HealthCheckException extends HttpException {
+    constructor(message: string) {
+        super(500, message);
+    }
+}
