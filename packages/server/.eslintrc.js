4ddcc36039f8aa288301acf78a03cbd0a32de397
Vladyslav Huz <wlad792@gmail.com>
2020-06-20 00:02:04 +0300

    [BE] bootstrap BE (alternative) - added few rules web package

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
+        'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended'
     ],
     parserOptions: {
         ecmaVersion: 2018,
@@ -9,6 +11,10 @@ module.exports = {
     },
     rules: {
         quotes: [2, 'single'],
-        'object-curly-spacing': [2]
+        'object-curly-spacing': [2],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error'
     },
 };
