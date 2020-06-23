61ad0bcb3e8e0431104fcb3bc058f2ffa7e188f5
chromogen <orfoline@gmail.com>
2020-06-23 16:43:49 +0300

    feture/8 mvp with deployed lambda

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,43 +1,44 @@
-import { Client, QueryResult } from "pg";
-import express from "express";
+import { Client, QueryResult } from 'pg';
+import express from 'express';
 
 const app = express();
 
-const config = {
-  user: "postgres",
-  password: "Z0hiR5D7BYA43bKJvnMY",
-  database: "postgres",
-  port: 5432,
-  host: "croton-global.cizhyv33sdan.us-east-2.rds.amazonaws.com",
+const getClientConfig = (parsed) => ({
+  user: parsed.POSTGRES_USER || 'postges',
+  password: parsed.POSTGRES_PASSWORD || 'qwerty1234',
+  database: parsed.POSTGRES_DB || 'postges',
+  port: Number(parsed.POSTGRES_PORT || '5432'),
+  host: parsed.POSTGRES_HOST || '127.0.0.1',
   statement_timeout: 10000,
   query_timeout: 10000,
-};
-
-type QueryToDB = (query: string) => Promise<QueryResult<any>>;
-
-const queryToDB: QueryToDB = async (query) => {
-  const client = new Client(config);
-  client.connect();
-  const queryRes = await client.query(query);
-  await client.end();
-  return queryRes;
-};
-
-app.get("/groupUser", async (_, res) => {
-  const queryRes = await queryToDB("SELECT * FROM groupuser");
-  res.send({
-    statusCode: 200,
-    body: queryRes,
-    headers: { "Content-Type": "application/json" },
-  });
 });
 
-app.get("/systemUser", async (_, res) => {
-  const queryRes = await queryToDB("SELECT * FROM systemuser");
+type QueryToDB = (query?: string) => Promise<string | QueryResult<any>>;
+
+const queryToDB: QueryToDB = async (query) => {
+  try {
+    const clientConfig = getClientConfig(global.process.env);
+    const client = new Client(clientConfig);
+
+    await client.connect();
+    let res = 'The DB is connected!';
+    let queryRes;
+    if (query) {
+      queryRes = await client.query(query);
+    }
+    await client.end();
+    return queryRes || res;
+  } catch (err) {
+    return `DB health check failed with following error: \n ${err}`;
+  }
+};
+
+app.get('/api/health-check', async (_, res) => {
+  const queryRes = await queryToDB();
   res.send({
     statusCode: 200,
     body: queryRes,
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 });
 
