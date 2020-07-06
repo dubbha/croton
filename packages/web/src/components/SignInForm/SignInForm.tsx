204f5e6a2d6c499411016d72847d4ca0da459cf6
Oleksiy Dubovyk <dubovyk@gmail.com>
2020-07-06 16:47:22 +0300

    partially restore SignIn

--- packages/web/src/components/SignInForm/SignInForm.tsx
+++ packages/web/src/components/SignInForm/SignInForm.tsx
@@ -1,13 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
+import { Form, SubmitButton, ErrorAlert, AlertPlaceholder, Link } from 'elements';
 import './styles.scss';
-
-import {
-  Form,
-  SubmitButton,
-  ErrorAlert,
-  AlertPlaceholder,
-  Link,
-} from 'elements';
 
 type Props = {
   isLoading: boolean;
@@ -32,33 +25,32 @@ export const SignInForm = ({ isLoading, error, onSubmit }: Props) => {
   return (
     <Form onSubmit={handleSubmit}>
       {error ? <ErrorAlert>{error}</ErrorAlert> : <AlertPlaceholder />}
-      {isLoading && 'Is Loading'}
       <Form.Group>
         <Form.Label>Email address</Form.Label>
         <Form.Control
           type="email"
           placeholder="Enter email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={e => setEmail(e.target.value)}
           data-testid="signInForm__email"
         />
       </Form.Group>
 
-      <Form.Group controlId="formPassword">
+      <Form.Group>
         <Form.Label>Password</Form.Label>
         <Form.Control
           type="password"
           placeholder="Enter password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={e => setPassword(e.target.value)}
           data-testid="signInForm__password"
         />
         <Form.Text className="reset-password-link">
           <Link to="/reset">Forgot password?</Link>
         </Form.Text>
       </Form.Group>
-      <SubmitButton disabled={!isValid} data-testid="submitButton">
-        Sign Up
+      <SubmitButton disabled={!isValid || isLoading} data-testid="submitButton">
+        Sign In
       </SubmitButton>
     </Form>
   );
