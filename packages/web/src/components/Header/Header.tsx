ba712dfe7940067b15fd2af6a25e0a818fdd9762
Karolina_Yanko <karolina_yanko@epam.com>
2020-07-08 12:38:45 +0300

    feat(38): (web) Added Home Page, updated Header, Footer, updated theme

--- packages/web/src/components/Header/Header.tsx
+++ packages/web/src/components/Header/Header.tsx
@@ -1,36 +1,56 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Navbar from 'react-bootstrap/Navbar';
 import { Nav, NavLink } from 'elements';
 import { getAuth } from 'store/auth/selectors';
 import './styles.scss';
+import logo from './logo.svg';
 
 export const Header = () => {
-  const { token } = useSelector(getAuth);
+  const { token, name } = useSelector(getAuth);
 
   return (
     <header>
-      <Nav>
-        <Nav.Item>
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-        </Nav.Item>
-        {token ? (
-          <NavLink to="/signout">Sign Out</NavLink>
-        ) : (
-          <>
-            <Nav.Item>
-              <NavLink to="/signin">Sign In</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink to="/signup">Sign Up</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink to="/reset">Reset Password</NavLink>
-            </Nav.Item>
-          </>
-        )}
-      </Nav>
+      <Navbar expand="lg">
+        <Nav className="mr-auto">
+          <Navbar.Brand href="/">
+            <img
+              alt="logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            <span className="companyName">Croton</span>
+          </Navbar.Brand>
+          <Nav.Item>
+            <Nav.Link href="#features">Features</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#services">Services</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Nav>
+          {token ? (
+            <>
+              <Nav.Item>{name}</Nav.Item>
+              <NavLink to="/signout">Sign Out</NavLink>
+            </>
+          ) : (
+            <>
+              <Nav.Item>
+                <NavLink to="/signin">Sign In</NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink to="/signup">Sign Up</NavLink>
+              </Nav.Item>
+            </>
+          )}
+        </Nav>
+      </Navbar>
     </header>
   );
 };
