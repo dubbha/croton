7c4b34729eb182d825e1b6fa3b2b0862001cc0ae
Oleksiy Dubovyk <dubovyk@gmail.com>
2020-07-13 20:14:03 +0300

    feat/43-WIP

---
+++ packages/web/server/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path');
+const http = require('http');
+const https = require('https');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+const { HOME, PORT = 80, HTTPS_PORT = 443 } = process.env;
+
+http.createServer({}, app)
+  .listen(PORT, function () {
+    console.log(`HTTP server listening at http://localhost:${PORT}/`)
+  })
+
+const certFiles = {
+  key: path.resolve(`${HOME}/server.key`),
+  cert: path.resolve(`${HOME}/server.cert`),
+};
+
+if (fs.existsSync(certFiles.key) && fs.existsSync(certFiles.cert)) {
+  const key = fs.readFileSync(certFiles.key);
+  const cert = fs.readFileSync(certFiles.cert);
+
+  https.createServer({ key, cert }, app)
+    .listen(HTTPS_PORT, function () {
+      console.log(`HTTPS server listening at https://localhost:${HTTPS_PORT}/`)
+    })
+}
+
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import store, { history } from 'store';
 import { Home, SignIn, SignUp, PasswordReset, Profile, SignOut, EmailConfirm } from 'pages';
 import './custom.scss';
@@ -9,13 +9,15 @@ import './custom.scss';
 const App = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Route path="/signin" component={SignIn} />
-      <Route path="/signout" component={SignOut} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/reset" component={PasswordReset} />
-      <Route path="/confirm" component={EmailConfirm} />
-      <Route path="/" exact component={Home} />
+      <Switch>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signout" component={SignOut} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/reset" component={PasswordReset} />
+        <Route path="/confirm" component={EmailConfirm} />
+        <Route path="/" component={Home} />
+      </Switch>
     </ConnectedRouter>
   </Provider>
 );
