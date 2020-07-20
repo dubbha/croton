46e8e9e42eebd368820c7f49ee032fa6bdd5ae93
Oleksiy Dubovyk <dubovyk@gmail.com>
2020-07-20 22:44:51 +0300

    feat(15): fix visuals, logo, home links, useScrollIntoView hook

--- packages/web/src/elements/navs/index.tsx
+++ packages/web/src/elements/navs/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
-import { NavLink as RouterNavLink } from 'react-router-dom';
+import {
+  NavLink as RouterNavLink,
+  Link as RouterLink,
+} from 'react-router-dom';
 
 export { default as Nav } from 'react-bootstrap/Nav';
-export { Link } from 'react-router-dom';
 
-type Props = {
-  to: string;
-  exact?: boolean;
+interface LinkProps {
+  to: string | object;
   children?: React.ReactNode | React.ReactNode[];
 }
-export const NavLink = ({ to, exact, children }: Props) =>
+
+export const Link = ({ to, children }: LinkProps) =>
+  <Nav.Link as={RouterLink} to={to}>{children}</Nav.Link>
+
+interface NavLinkProps extends LinkProps {
+  exact?: boolean;
+}
+
+export const NavLink = ({ to, exact, children }: NavLinkProps) =>
   <Nav.Link as={RouterNavLink} to={to} exact={exact}>{children}</Nav.Link>
+
