277584b7002ec284cd82e1f82533a53dc80356e3
belgithub <belinua@gmail.com>
2020-07-21 16:35:09 +0300

    Feat/11 sign up refactoring (#50)
    * feat(11): custom form hook refactoring
    * feat(11): provided strict types
    
    Co-authored-by: Oleksandr Biliaiev <Oleksandr_Biliaiev@epam.com>

--- packages/web/src/services/http/http.service.ts
+++ packages/web/src/services/http/http.service.ts
@@ -8,7 +8,6 @@ export const http = axios.create({
 http.interceptors.request.use(
   config => {
     const token = localStorage.getItem('authToken');
-    console.log('token', token);
     if (token) {
       config.headers.authorization = token; // eslint-disable-line no-param-reassign
     }
