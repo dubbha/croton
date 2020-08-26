6ccfd6a681725b62c6b45e77abec946b2200ceb9
Vladyslav Huz <wlad792@gmail.com>
2020-08-27 01:01:34 +0300

    fix(22): added disconnection from db

--- packages/server/src/utils/createPostgresConnection.ts
+++ packages/server/src/utils/createPostgresConnection.ts
@@ -1,18 +1,19 @@
 import 'dotenv/config';
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
+import {Connection, createConnection} from 'typeorm';
 
 import { validateEnv } from './validate-env';
 
 import config from './ormconfig';
 
-export default async (): Promise<void> => {
+export default async (): Promise<Connection> => {
   console.log('Validating env params...');
   validateEnv();
   try {
     console.log('Connecting to DB...');
-    await createConnection(config);
+    const connection = await createConnection(config);
     console.log('Connected to DB!');
+    return connection;
   } catch (error) {
     console.log('Error while connecting to the DB:', error);
     return error;
