21f8081866db2f9081779a1c955ec4d98c4a2942
Artem S <svart0222@gmail.com>
2020-09-02 21:29:23 +0300

    feat(66): add integration with firebase for notification

--- packages/mobile/src/App.test.tsx
+++ packages/mobile/src/App.test.tsx
@@ -6,6 +6,13 @@ import 'react-native';
 import React from 'react';
 
 jest.mock('react-native-push-notification', () => ({ configure: jest.fn() }));
+jest.mock('@react-native-community/push-notification-ios', () => {
+  return {
+    addEventListener: jest.fn(),
+    requestPermissions: jest.fn(() => Promise.resolve()),
+    getInitialNotification: jest.fn(() => Promise.resolve()),
+  };
+});
 jest.mock('react-native-gesture-handler');
 import { App } from './App';
 
