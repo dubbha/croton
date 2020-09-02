a2fe080fd949ad56824b677f33c936c99b80771b
Artem S <svart0222@gmail.com>
2020-09-02 21:29:23 +0300

    feat(66): add integration with firebase for notification

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 import store from './store';
 import { MainScreen } from './pages/Main';
@@ -14,6 +15,24 @@ PushNotification.configure({
   onNotification: function(notification: any) {
     console.log('NOTIFICATION:', notification);
   },
+});
+
+// TODO: this is changes for test with real dev account
+PushNotificationIOS.requestPermissions(['alert', 'badge', 'sound']);
+
+PushNotificationIOS.addEventListener('register', function(data) {
+  console.log('PushNotificationIOS register');
+  console.dir(data);
+});
+
+PushNotificationIOS.addEventListener('notification', function(data) {
+  console.log('PushNotificationIOS notification');
+  console.dir(data);
+});
+
+PushNotificationIOS.addEventListener('registrationError', function(data) {
+  console.log('PushNotificationIOS registrationError');
+  console.dir(data);
 });
 
 export const App = () => {
