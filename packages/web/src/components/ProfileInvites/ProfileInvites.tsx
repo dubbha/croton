5efe941a9b7c973a9638c78764a7842dc0f4ec6e
Oleksiy Dubovyk <dubovyk@gmail.com>
2020-09-11 00:00:57 +0300

    fix: minor invites bugs after rebase

--- packages/web/src/components/ProfileInvites/ProfileInvites.tsx
+++ packages/web/src/components/ProfileInvites/ProfileInvites.tsx
@@ -23,11 +23,12 @@ export const ProfileInvites = () => {
     dispatch({ type: AUTH_GET_INVITES });
   }, [dispatch, invites.length]);
 
+  const reloadInvites = () => { dispatch({ type: AUTH_GET_INVITES }); };
+
   const handleClose = () => {
+    reloadInvites();
     setShowModal(false);
   };
-
-  const reloadInvites = () => { dispatch({ type: AUTH_GET_INVITES }); };
 
   const calculateHours = (expiresIn: string) => {
     const dateNow = new Date().getTime();
@@ -47,7 +48,7 @@ export const ProfileInvites = () => {
 
   return (
     <>
-      {invites.length && <Button variant="link" className="profile-link" onClick={() => handleOpen()}>Pending Invites ({invites.length})</Button>}
+      {!!invites.length && <Button variant="link" className="profile-link" onClick={() => handleOpen()}>Pending Invites ({invites.length})</Button>}
       <Modal show={showModal} onHide={handleClose} size="lg" centered>
         <Modal.Header closeButton>
           <Modal.Title>
@@ -59,36 +60,42 @@ export const ProfileInvites = () => {
             {info && <InfoAlert>{info}</InfoAlert>}
             {error && <ErrorAlert>{error}</ErrorAlert>}
           </div>
-          {invites.length ? (
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>Shelf Name</th>
-                  <th>Shelf Location</th>
-                  <th>Expires In</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {invites.map(({ id, shelf, expiresIn, shelfInvitationToken }) => (
-                  <tr key={id}>
-                    <td>{shelf.name}</td>
-                    <td>{shelf.location}</td>
-                    <td>{calculateHours(expiresIn)} hours</td>
-                    <td>
-                      <Button
-                        disabled={isLoading}
-                        variant="outline-success"
-                        onClick={() => acceptInvite(shelfInvitationToken)}
-                      >
-                        Accept
-                      </Button>
-                    </td>
+          {invites.length
+            ? (
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>Shelf Name</th>
+                    <th>Shelf Location</th>
+                    <th>Expires In</th>
+                    <th>Actions</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          ) : <div className="mt-2 mb-2"><InfoAlert>You do not have any invites yet.</InfoAlert></div>}
+                </thead>
+                <tbody>
+                  {invites.map(({ id, shelf, expiresIn, shelfInvitationToken }) => (
+                    <tr key={id}>
+                      <td>{shelf.name}</td>
+                      <td>{shelf.location}</td>
+                      <td>{calculateHours(expiresIn)} hours</td>
+                      <td>
+                        <Button
+                          disabled={isLoading}
+                          variant="outline-success"
+                          onClick={() => acceptInvite(shelfInvitationToken)}
+                        >
+                          Accept
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )
+            : !info && (
+              <div className="mt-2 mb-2">
+                <InfoAlert>You do not have any invites yet.</InfoAlert>
+              </div>
+            )}
         </Modal.Body>
       </Modal>
     </>
